dc2e7e08ad27be483948fb989d432b9c3648c310
RAHUL KUMAR <112709761+MEGAMINDSPARK@users.noreply.github.com>
2023-08-22 09:45:18 +0530

    Create ExpenseItem.js

---
+++ ExpenseItem.js
@@ -0,0 +1,15 @@
+
+function ExpenseItem() {
+    // this is our first basic component
+    // return <h2>Expense Item</h2>
+    // and to use this component we need to export this
+    return (
+      <>
+        <p>Expense Item</p>
+        <p>Food RS 10</p>
+        <p>Petrol RS 100</p>
+        <p>Movies RS 200</p>
+      </>
+    );
+  }
+  export default ExpenseItem;
